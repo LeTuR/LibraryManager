459d7577f2657b33c405e46a8f3746136981515b
Arthur Cesare <letur@live.fr>
2018-04-12 10:17:43 +0200

    Initial Commit

---
+++ LibraryManager/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LibraryManager
+//
+//  Created by MagicLetur on 12/04/2018.
+//  Copyright © 2018 Cesare-Herriau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
