a8218c54739b714c1eb3da72c98934c588cf48b0
LeTuR <38325495+LeTuR@users.noreply.github.com>
2018-04-25 10:20:51 +0200

    Add files via upload

--- LibraryManager/main.cpp
+++ LibraryManager/main.cpp
@@ -8,8 +8,27 @@
 
 #include <iostream>
 
+using namespace std;
+
+
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	string input;
+	cout << "Welcome to the Library Manager:\n";
+	cout << "Please enter a command, to see the list of commands type HELP, if you want to exit type EXIT\n"<<"\n";
+	for(;;){
+		cin>>input;
+		if (input == "HELP"){
+			cout << "--------------------\n";
+			cout << "List of commands:\n";
+			cout << "BYE   ADD     LOAD\n";
+			cout << "SAVE  SEARCH  CLEAR\n";
+			cout << "LIST  SHOW    DELETE\n";
+			cout << "RESET HELP    EXIT\n";
+			cout << "--------------------\n"<<"\n";
+			cout << "Please enter a command, to see the list of commands type HELP, if you want to exit type EXIT\n";
+
+
+		}
+	}
 	return 0;
 }
